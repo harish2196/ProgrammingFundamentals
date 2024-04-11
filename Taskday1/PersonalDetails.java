d2bb45f80cfb3620fb69c032a32b32d4d342f52e
harish2196 <144200799+harish2196@users.noreply.github.com>
2024-04-11 15:25:34 +0530

    Task 1

---
+++ Taskday1/PersonalDetails.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.chainsys.day1;
+
+
+public class PersonalDetails {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("First name:Harish\nLast name:kumar\nDept:CSE\nLocality:Sivakasi");
+		
+
+
+	}
+
+}
